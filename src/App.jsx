1adb2f11f801ec7b0965b3447a63e82a9be54d75
Layla Rodas <rodas.layla@gmail.com>
2024-03-04 16:42:38 +0100

    start project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <h1>Appointments App with React</h1>
+      <Header />
+    </>
+  )
+}
+
+export default App
